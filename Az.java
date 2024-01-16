4af6bb0aa118ef7b959331407d66b817fb788540
Gourav Chaudhary <gouravsittam@gmail.com>
2024-01-16 10:40:43 +0530

    update

--- Az.java
+++ Az.java
@@ -1,5 +1,7 @@
 public class Az {
     public static void main(String[] args) {
         System.out.println("Hello Class AZ");
+        System.out.println("How are you");
+        System.out.println("I Hope everything is okay");
     }
 }
