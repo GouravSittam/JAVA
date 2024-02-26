9f21810658a50849cd69d89cdfb0c854f61323dd
Gourav Chaudhary <gouravsittam@gmail.com>
2024-02-26 18:03:22 +0530

    up

---
+++ bit.java
@@ -0,0 +1,17 @@
+public class bit {
+    public static void main(String[] args) {
+        int a=9,b=10;
+        System.out.println("Given numbers output are: "+(a&b));
+        System.out.println("Given numbers output are: "+(a|b));
+        System.out.println("Given numbers output are: "+(a^b));
+        System.out.println("Given numbers output are: "+(a<<1));
+        System.out.println("Given numbers output are: "+(a<<2));
+        System.out.println("Given numbers output are: "+(b<<1));
+        System.out.println("Given numbers output are: "+(b<<2));
+        System.out.println("Given numbers output are: "+(a>>1));
+        System.out.println("Given numbers output are: "+(a>>2));
+        System.out.println("Given numbers output are: "+(b>>1));
+        System.out.println("Given numbers output are: "+(b>>2));
+    }
+    
+}
