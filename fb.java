8915eba48fe01d67e5b5c26d6d427200c03e827b
Gourav Chaudhary <gouravsittam@gmail.com>
2024-03-19 10:27:46 +0530

    new prog

--- fb.java
+++ fb.java
@@ -1,19 +1,20 @@
 class A {
+
     static int x = 10;
     int y = 20;
     private static int z = 30;
 
     static class B {
         void get() {
-            System.out.println("x:" + x);
-            System.out.println("z:" + z);
-        }
-    }
-
-    public static class fb { 
-        public static void main(String[] args) {
-            A.B obj = new A.B(); 
-            obj.get();
+            System.out.print( "X :" + x);
+            System.out.print("Z :" + z);
         }
     }
 }
+
+    public class fb { // Moved FirstProgram to a public static class
+        public static void main (String[] args) { // Corrected main method signature
+            A.B obj = new A.B(); // Corrected class reference
+            obj.get();
+        }
+    }
