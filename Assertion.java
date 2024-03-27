ddde58fe3a0480e5ecca67335e5c4d700fbe122c
Gourav Chaudhary <gouravsittam@gmail.com>
2024-03-27 12:39:32 +0530

    update

---
+++ Assertion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Assertion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int value = sc.nextInt();
+        assert value >= 10 : "Age must be 10 or older";
+        System.out.println("Value is " + value);
+    }
+}
